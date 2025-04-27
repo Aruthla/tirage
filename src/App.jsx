d7291f1618c8fa9759d6a31f76ea15397b6c0978
Laëtitia Cliquennois <laetitia.cli@live.fr>
2025-04-27 20:02:58 +0200

    MAJ

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormContainer from './components/FormContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <FormContainer />
+    </div>
+  );
+}
+
+export default App;
